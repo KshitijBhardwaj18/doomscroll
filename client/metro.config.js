41a96b8a2d452e3534cdeb3746a95f0fdda88c9f
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2025-10-07 21:28:50 +0530

    add:(client) solana mobile wallet connection boilerplate code

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,8 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./app/global.css" });
+// Add polyfill resolvers
+config.resolver.extraNodeModules.crypto = require.resolve('expo-crypto');
+
+module.exports = config;
